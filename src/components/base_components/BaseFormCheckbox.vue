6923ba818bcca020bcee433b985c26978a201051
Maksim Fedotov <doutatsu@gmail.com>
2020-04-16 12:51:56 +0100

    Create BaseFormCheckbox component
    This a simple checkbox component. I will improve this
    going forward, to add intermediary state as well

---
+++ src/components/base_components/BaseFormCheckbox.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+  .relative.flex.items-start
+    .absolute.flex.items-center.h-5
+      input.form-checkbox#checkbox(
+        type='checkbox'
+        v-bind:checked="value"
+        v-on:input="$emit('input', $event.target.checked)"
+      )
+    .pl-7.text-sm.leading-5(v-if='$slots.default')
+      label.font-medium.text-gray-700(for="checkbox")
+        slot
+</template>
+
+<script>
+  export default {
+    props: {
+      value: {
+        type: Boolean,
+        required: true,
+      },
+      label: {
+        type: String,
+        default: null,
+      },
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  input {
+    @apply h-4 w-4 text-blue-600 transition duration-150 ease-in-out;
+  }
+</style>
