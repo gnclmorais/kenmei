7fbd5cfe232b857550df045ad199fd8f42bd6b8b
Maksim Fedotov <doutatsu@gmail.com>
2020-05-03 16:28:35 +0100

    Add new colours for Down state

--- src/views/Sources.vue
+++ src/views/Sources.vue
@@ -48,6 +48,8 @@
           return 'bg-green-200 text-green-800';
         case 'In Development':
           return 'bg-yellow-200 text-yellow-800';
+        case 'Down':
+          return 'bg-red-200 text-red-800';
         default:
           return 'bg-gray-200 text-gray-800';
         }
