020dbb3be3886d35ba754d80eb320b25f9338343
doutatsu <doutatsu@gmail.com>
2020-05-24 12:36:30 +0100

    Remove ProgressBar
    Tests were failing after the yarn lock update, so I thought
    I might as well delete this component, as I don't plan on
    using it in the future

--- tests/components/ProgressBar.spec.js
+++
@@ -1,9 +0,0 @@
-import ProgressBar from '@/components/ProgressBar.vue';
-
-describe('ProgressBar.vue', () => {
-  it('renders progress bar', () => {
-    const mangaList = shallowMount(ProgressBar);
-
-    expect(mangaList.html()).toMatchSnapshot();
-  });
-});
