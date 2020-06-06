e74a2b1208668dae3206616473c1c308b7a7dda6
doutatsu <doutatsu@gmail.com>
2020-06-06 12:45:02 +0100

    Add update and destroy endpoints to registrations

--- src/services/endpoints/auth/registrations.js
+++ src/services/endpoints/auth/registrations.js
@@ -1,10 +1,16 @@
-import { plain } from '@/modules/axios';
+import { plain, secure } from '@/modules/axios';
 
 const baseURL = '/auth/registrations';
 
-/* eslint-disable import/prefer-default-export */
 export const create = (user) => plain
   .post(baseURL, { user })
   .then((request) => request)
   .catch((request) => request.response);
-/* eslint-enable import/prefer-default-export */
+export const update = (user) => plain
+  .put(`${baseURL}/${user.id}`, { user: { email: user.email } })
+  .then((request) => request)
+  .catch((request) => request.response);
+export const destroy = (id) => secure
+  .delete(`${baseURL}/${id}`)
+  .then((request) => request)
+  .catch((request) => request.response);
