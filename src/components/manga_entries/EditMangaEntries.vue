ddd0a68194025f03b707642065b45ab68dbcc65c
doutatsu <doutatsu@gmail.com>
2020-06-14 14:03:32 +0100

    Update EditMangaEntries
    Adds ability to set tags

--- src/components/manga_entries/EditMangaEntries.vue
+++ src/components/manga_entries/EditMangaEntries.vue
@@ -34,6 +34,22 @@
                 :label="source.name"
                 :value="source.id"
               )
+        .mt-5.text-center.sm_text-left.w-full(v-if="tags.length")
+          label.block.text-sm.leading-5.font-medium.text-gray-700
+            | Tags
+          .mt-1.relative.rounded-md.shadow-sm.w-auto
+            el-select.rounded.w-full(
+              v-model="selectedTagIDs"
+              placeholder="Select Tags"
+              multiple
+              collapse-tags
+            )
+              el-option(
+                v-for="tag in tags"
+                :key="tag.id"
+                :label="tag.name"
+                :value="tag.id"
+              )
     template(slot='actions')
       span.sm_ml-3.flex.w-full.rounded-md.shadow-sm.sm_w-auto
         base-button(
@@ -71,6 +87,7 @@
     data() {
       return {
         selectedStatus: 1,
+        selectedTagIDs: [],
         mangaSourceID: null,
         availableSources: [],
         loadingSources: false,
@@ -80,12 +97,20 @@
     computed: {
       ...mapState('lists', [
         'statuses',
+        'tags',
       ]),
       selectedEntry() {
         return this.selectedEntries[0];
       },
       selectedEntriesIDs() {
         return this.selectedEntries.map((entry) => entry.id);
+      },
+      selectedEntriesTagIDs() {
+        if (!this.selectedEntries.length) return [];
+
+        const tagIDs = this.selectedEntries.map((entry) => entry.user_tag_ids);
+
+        return [].concat(...tagIDs);
       },
       isBulkUpdate() {
         return this.selectedEntries.length > 1;
@@ -94,6 +119,8 @@
     watch: {
       selectedEntries(entries, oldEntries) {
         if (entries.length) {
+          this.selectedTagIDs = this.selectedTags();
+
           if (entries !== oldEntries && !this.isBulkUpdate) {
             this.selectedStatus = this.selectedEntry.attributes.status;
           }
@@ -115,6 +142,11 @@
       toggleLoading() {
         this.loadingSources = !this.loadingSources;
       },
+      selectedTags() {
+        return this.tags.filter(
+          (tag) => this.selectedEntriesTagIDs.includes(tag.id)
+        ).map((tag) => tag.id);
+      },
       async loadAvailableSources() {
         this.toggleLoading();
 
@@ -135,7 +167,10 @@
       },
       async updateMangaEntries() {
         this.loading = true;
-        const params  = { status: this.selectedStatus };
+        const params  = {
+          status: this.selectedStatus,
+          user_tag_ids: this.selectedTagIDs,
+        };
 
         if (!this.isBulkUpdate) { params.manga_source_id = this.mangaSourceID; }
 
