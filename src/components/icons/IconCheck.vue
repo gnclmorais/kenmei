039485c29d9347fcbb67cde399e3d2650cc0d0fa
doutatsu <doutatsu@gmail.com>
2020-07-04 13:07:00 +0100

    Add IconCheck svg component

---
+++ src/components/icons/IconCheck.vue
@@ -0,0 +1,9 @@
+<template lang="pug">
+  svg.stroke-current(fill='none' viewbox='0 0 24 24')
+    path(
+      stroke-linecap='round'
+      stroke-linejoin='round'
+      stroke-width='2'
+      d='M5 13l4 4L19 7'
+    )
+</template>
