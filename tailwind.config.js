7c1bb7e32b03bcabf8ccf26ae88162d28d1f6d52
doutatsu <doutatsu@gmail.com>
2020-08-24 20:44:59 +0100

    Purge unused tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   separator: '_',
+  purge: [
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.jsx',
+  ],
   theme: {
     screens: {
       sm: '640px',
