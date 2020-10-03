ee3450005921216db95191da52286446484f1464
doutatsu <doutatsu@gmail.com>
2020-10-03 17:29:31 +0100

    SettingsAccountPasswordSection uses base input

--- tests/components/settings/SettingsAccountPasswordSection.spec.js
+++ tests/components/settings/SettingsAccountPasswordSection.spec.js
@@ -1,3 +1,4 @@
+import i18n from 'i18n';
 import { Message } from 'element-ui';
 import flushPromises from 'flush-promises';
 import AccountPasswordSection from '@/components/settings/SettingsAccountPasswordSection.vue';
@@ -12,11 +13,21 @@ describe('AccountPasswordSection.vue', () => {
   let passwordSection;
 
   beforeEach(() => {
-    passwordSection = mount(AccountPasswordSection, { localVue });
+    passwordSection = mount(AccountPasswordSection, { localVue, i18n });
   });
 
   describe('when pressing Save button', () => {
     describe('and attributes are valid', () => {
+      beforeEach(() => {
+        passwordSection.setData({
+          user: {
+            password: 'password1',
+            passwordConfirmation: 'password1',
+            currentPassword: 'password',
+          },
+        });
+      });
+
       it('shows Password updated messange', async () => {
         const successMessageSpy = jest.spyOn(Message, 'success');
         const updatePasswordSpy = jest.spyOn(endpoint, 'update');
@@ -32,7 +43,21 @@ describe('AccountPasswordSection.vue', () => {
     });
 
     describe('and attributes are invalid', () => {
+      it('shows client-side validation errors', async () => {
+        await passwordSection.find('button').trigger('click');
+
+        expect(passwordSection.text()).toContain('Current passwordrequired');
+      });
+
       it('shows server-side validation errors', async () => {
+        passwordSection.setData({
+          user: {
+            password: 'password1',
+            passwordConfirmation: 'password1',
+            currentPassword: 'password',
+          },
+        });
+
         const errorMessageSpy   = jest.spyOn(Message, 'error');
         const updatePasswordSpy = jest.spyOn(endpoint, 'update');
         updatePasswordSpy.mockResolvedValue({
