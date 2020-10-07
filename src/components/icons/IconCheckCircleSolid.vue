ea74ecab8f4f31171c10689644592761a303431f
doutatsu <doutatsu@gmail.com>
2020-10-07 12:56:29 +0100

    Add new icons

---
+++ src/components/icons/IconCheckCircleSolid.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  svg(fill='currentColor' viewbox='0 0 20 20')
+    path(fill-rule='evenodd' :d='d' clip-rule='evenodd')
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        d: `
+          M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9
+          10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z
+        `,
+      };
+    },
+  };
+</script>
