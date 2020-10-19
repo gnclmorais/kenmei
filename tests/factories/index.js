c2b920c6645a9132e971888d880eb0c5ea440a46
doutatsu <doutatsu@gmail.com>
2020-10-19 21:52:06 +0100

    Update Factories
    - Add mangaSeries factory
    - Update mangaSource factory to contain new fields

--- tests/factories/index.js
+++ tests/factories/index.js
@@ -1,6 +1,12 @@
 import entry from './mangaEntry';
 import userTag from './UserTag';
 import source from './mangaSource';
+import series from './mangaSeries';
 
 // eslint-disable-next-line import/prefer-default-export
-export const factories = { entry, source, userTag };
+export const factories = {
+  entry,
+  source,
+  series,
+  userTag,
+};
--- tests/factories/mangaSource.js
+++ tests/factories/mangaSource.js
@@ -4,4 +4,7 @@ export default Factory.define(({ sequence }) => ({
   id: sequence,
   manga_series_id: 1,
   name: 'MangaDex',
+  seriesURL: 'example.com',
+  lastVolumeAvailable: 1,
+  lastChapterAvailable: 2,
 }));
